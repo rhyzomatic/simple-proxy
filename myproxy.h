8b5c42c5104e23571f83e38b95cf6b1d09b67166
Gary Chan <GaryChan@Gary-MacBookPro.local>
2016-03-21 00:38:05 +0800

    no message

--- myproxy.h
+++ myproxy.h
@@ -1,9 +1,28 @@
 // myproxy.h
 //
-
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <cerrno>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <dirent.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <crypt.h>
+#include <algorithm>
 #ifndef LZZ_myproxy_h
 #define LZZ_myproxy_h
 #define LZZ_INLINE inline
+
+using namespace std;
+
 void error_handler (string error_string);
 int send_all (int client_socket, unsigned char * buffer, int length);
 int rec_all (int client_socket, char * payload, int length);
@@ -15,6 +34,5 @@ pair <string,int> get_hostname_and_port (string & header);
 void pass_along_request (int client_socket, string & header);
 void parse_client_header (int client_socket, string & header);
 void * connection_handler (void * client_socket_ptr);
-int main (int argc, char * (argv) []);
 #undef LZZ_INLINE
 #endif
